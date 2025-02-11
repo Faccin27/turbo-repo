fde576c237e8e52cf2821064f72c4d51f7eebc53
Guilherme Faccin <gfaccin27@gmail.com>
2025-02-11 17:26:22 -0300

    feat: created main page

---
+++ apps/projeto1/src/components/main.tsx
@@ -0,0 +1,10 @@
+
+const Main = () => {
+  return (
+    <div className="flex gap-4 p-4 bg-gray-800 text-white">
+      <h1 className="text-6xl text-red-500 font-bold">BEM VINDO AO GPTEISON</h1>
+    </div>
+  );
+};
+
+export default Main;
